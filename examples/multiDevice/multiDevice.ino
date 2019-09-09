042488c2a2bb6df3785df06e55f5628f41005a3f
Alex Bustos <alexfbp@gmail.com>
2019-09-09 02:31:52 -0500

    Using bitfields, Wire support added

--- examples/multiDevice/multiDevice.ino
+++ examples/multiDevice/multiDevice.ino
@@ -1,5 +1,6 @@
 #include "MLX90615.h"
-#include <I2cMaster.h>
+
+// Way 1: Using additional pins
 
 byte sda_1 = 3;
 byte scl_1 = 2;
@@ -17,10 +18,34 @@ MLX90615 mlx90615_2(DEVICE_ADDR, &i2c_2);
 SoftI2cMaster i2c_3(sda_3, scl_3);
 MLX90615 mlx90615_3(DEVICE_ADDR, &i2c_3);
 
+// // Way 2: If you have set your MLX90615s up to have unique
+// // addresses, like in the "changeAddr.ino" sketch, you can
+// // have all your MLX90615 sharing same pins/bus
+
+// // Update with your real addresses
+// #define DEVICE1_ADDR DEVICE_ADDR
+// #define DEVICE2_ADDR DEVICE_ADDR+1
+// #define DEVICE3_ADDR DEVICE_ADDR+2
+
+// // // Using included I2C lib
+// // #define SDA_PIN SDA   //define the SDA pin
+// // #define SCL_PIN SCL   //define the SCL pin
+// // SoftI2cMaster i2c_bus(SDA_PIN, SCL_PIN);
+// // MLX90615 mlx90615_1(DEVICE1_ADDR, &i2c_bus);
+// // MLX90615 mlx90615_2(DEVICE2_ADDR, &i2c_bus);
+// // MLX90615 mlx90615_3(DEVICE3_ADDR, &i2c_bus);
+
+// // Using Wire
+// MLX90615 mlx90615_1(DEVICE1_ADDR, &Wire);
+// MLX90615 mlx90615_2(DEVICE2_ADDR, &Wire);
+// MLX90615 mlx90615_3(DEVICE3_ADDR, &Wire);
+
 
 void setup()
 {
   Serial.begin(9600);
+  while(!Serial); // Only for native USB serial
+  delay(2000); // Additional delay to allow open the terminal to see setup() messages
   Serial.println("Setup...");
 }
 
@@ -35,21 +60,21 @@ void loop()
   
   Serial.print("Temp_1: ");
   Serial.print(temperatureObj1);
-  Serial.print("`C  ");
+  Serial.print("°C  ");
   Serial.print(temperatureAmb1);
-  Serial.println("`C  ");
+  Serial.println("°C  ");
   
   Serial.print("Temp_2: ");
   Serial.print(temperatureObj2);
-  Serial.print("`C  ");
+  Serial.print("°C  ");
   Serial.print(temperatureAmb2);
-  Serial.println("`C  ");
+  Serial.println("°C  ");
   
   Serial.print("Temp_3: ");
   Serial.print(temperatureObj3);
-  Serial.print("`C  ");
+  Serial.print("°C  ");
   Serial.print(temperatureAmb3);
-  Serial.println("`C  ");
+  Serial.println("°C  ");
   
   Serial.println("\n=======================================\n\r");
   
