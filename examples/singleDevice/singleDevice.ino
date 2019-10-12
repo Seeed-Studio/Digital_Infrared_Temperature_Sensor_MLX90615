2eed38a8fe98715ef8c72561ece48a960a6169a8
Alex Bustos <alexfbp@gmail.com>
2019-10-12 12:18:22 -0500

    Examples reviewed, compilation flags added

--- examples/singleDevice/singleDevice.ino
+++ examples/singleDevice/singleDevice.ino
@@ -2,16 +2,23 @@
 
 #define MLX MLX90615_DefaultAddr
 
-// Uncomment this for included I2C library (may
-// interfer if you use other I2C devices in same
-// pins and their implementations use Wire)
-// #define SDA_PIN SDA   //define the SDA pin
-// #define SCL_PIN SCL   //define the SCL pin
-// SoftI2cMaster i2c(SDA_PIN, SCL_PIN);
-// MLX90615 mlx90615(MLX, &i2c);
+/*
+ * Uncomment the following line to use included I2C library
+ *
+ * Avoid (keep commented) if your I2C bus is being shared with
+ * other devices: they may be using Wire as its basis,
+ * incompatible with the included I2C library
+ */
+// #define INCLUDED_I2C
 
-// Comment this for old way (with included I2C lib.)
-MLX90615 mlx90615(MLX, &Wire);
+#ifdef INCLUDED_I2C
+  #define SDA_PIN SDA   //define the SDA pin
+  #define SCL_PIN SCL   //define the SCL pin
+  SoftI2cMaster i2c(SDA_PIN, SCL_PIN);
+  MLX90615 mlx90615(MLX, &i2c);
+#else // Using Wire
+  MLX90615 mlx90615(MLX, &Wire);
+#endif // INCLUDED_I2C not defined
 
 void setup()
 {
@@ -20,8 +27,9 @@ void setup()
   delay(2000); // Additional delay to allow open the terminal to see setup() messages
   Serial.println("Setup...");
 
-  // Comment this for old way (with included I2C lib.)
-  Wire.begin();
+  #ifndef INCLUDED_I2C // If using Wire:
+    Wire.begin();
+  #endif // INCLUDED_I2C not defined
 
   // // write data into EEPROM when you need to adjust emissivity.
   // Serial.println(mlx90615.writeReg(MLX90615_EEPROM_EMISSIVITY,0x0000)); // Erase! (and see result)
